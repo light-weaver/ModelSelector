5bf05ecc7aadeaae2cbc0b4d692ac336cd0997b0
bhupinder@home <bhupinder.saini2.71@gmail.com>
2019-07-01 18:22:50 +0300

    Generationfile completed

--- modelselector/generateData.py
+++ modelselector/generateData.py
@@ -9,6 +9,20 @@ from optproblems import wfg, cec2005, cec2007, dtlz, zdt
 from pyDOE import lhs
 from scipy.stats.distributions import norm
 
+mean = 0.5
+std_dev = 0.1
+noise_mean = 0
+noise_std = 0.05
+num_obj = 2
+num_var_zdt = {
+    "ZDT1": 30,
+    "ZDT2": 30,
+    "ZDT3": 30,
+    "ZDT4": 10,
+    "ZDT6": 10,
+}
+
+
 problems = {
     "WFG1": wfg.WFG1,
     "WFG2": wfg.WFG2,
@@ -42,6 +56,7 @@ def generatedata(
     distribution: str,
     noise: bool,
     missing_data: bool,
+    save_folder: str,
 ):
     """Generate random dataset from known benchmark problems or engineering problems.
 
@@ -59,17 +74,226 @@ def generatedata(
         Presence or absence of noise in data
     missing_data : bool
         Presence or absence of missing chunks of data
+    save_folder : str
+        Path to the save folder
     """
     if "DTLZ" in problemname:
         generateDTLZ(
-            problemname, num_var, num_samples, distribution, noise, missing_data
+            problemname,
+            num_var,
+            num_samples,
+            distribution,
+            noise,
+            missing_data,
+            save_folder,
         )
+        return
     elif "WFG" in problemname:
         generateWFG(
-            problemname, num_var, num_samples, distribution, noise, missing_data
+            problemname,
+            num_var,
+            num_samples,
+            distribution,
+            noise,
+            missing_data,
+            save_folder,
         )
+        return
     elif "ZDT" in problemname:
         generateZDT(
-            problemname, num_var, num_samples, distribution, noise, missing_data
+            problemname,
+            num_var,
+            num_samples,
+            distribution,
+            noise,
+            missing_data,
+            save_folder,
         )
-    pass
+        return
+    else:
+        print("Error with Problem name")
+    return
+
+
+def generate_var_0_1(
+    problemname: str,
+    num_var: int,
+    num_samples: int,
+    distribution: str,
+    noise: bool,
+    missing_data: bool,
+    save_folder: str,
+):
+    filename = (
+        save_folder
+        + "/"
+        + problemname
+        + "_"
+        + str(num_var)
+        + "_"
+        + str(num_samples)
+        + "_"
+        + distribution
+    )
+    if distribution == "Uniform":
+        var = lhs(num_var, num_samples)
+    elif distribution == "Normal":
+        means = [mean] * num_var
+        cov = np.eye(num_var) * np.square(std_dev)
+        var = np.random.multivariate_normal(means, cov, num_samples)
+    if noise:
+        noise_means = [noise_mean] * num_var
+        noise_cov = np.eye(num_var) * np.square(noise_std)
+        noise_var = np.random.multivariate_normal(noise_means, noise_cov, num_samples)
+        filename = filename + "_noisy"
+        var = var + noise_var
+    # To keep values between 0 and 1
+    var[var > 1] = 1
+    var[var < 0] = 0
+    return (var, filename)
+
+
+
+def generateDTLZ(
+    problemname: str,
+    num_var: int,
+    num_samples: int,
+    distribution: str,
+    noise: bool,
+    missing_data: bool,
+    save_folder: str,
+):
+    """Generate and save DTLZ datasets as csv.
+
+    Parameters
+    ----------
+    problemname : str
+        Name of the problem
+    num_var : int
+        number of variables
+    num_samples : int
+        Number of samples
+    distribution : str
+        Uniform or normal distribution
+    noise : bool
+        Presence or absence of noise
+    missing_data : bool
+        Presence or absence of missing data
+    save_folder : str
+        Path to the folder to save csv files
+    """
+    objective = problems[problemname](num_obj, num_var)
+    var_names = ["x{0}".format(x) for x in range(num_var)]
+    obj_names = ["f1", "f2"]
+    var, filename = generate_var_0_1(
+        problemname,
+        num_var,
+        num_samples,
+        distribution,
+        noise,
+        missing_data,
+        save_folder,
+    )
+    obj = [objective(x) for x in var]
+    data = np.hstack((var, obj))
+    data = pd.DataFrame(data, columns=var_names + obj_names)
+    filename = filename + ".csv"
+    data.to_csv(filename, index=False)
+    return
+
+
+def generateWFG(
+    problemname: str,
+    num_var: int,
+    num_samples: int,
+    distribution: str,
+    noise: bool,
+    missing_data: bool,
+    save_folder: str,
+):
+    """Generate and save WFG datasets as csv.
+
+    Parameters
+    ----------
+    problemname : str
+        Name of the problem
+    num_var : int
+        number of variables
+    num_samples : int
+        Number of samples
+    distribution : str
+        Uniform or normal distribution
+    noise : bool
+        Presence or absence of noise
+    missing_data : bool
+        Presence or absence of missing data
+    save_folder : str
+        Path to the folder to save csv files
+    """
+    objective = problems[problemname](num_obj, num_var, k=4)
+    var_names = ["x{0}".format(x) for x in range(num_var)]
+    obj_names = ["f1", "f2"]
+    var, filename = generate_var_0_1(
+        problemname,
+        num_var,
+        num_samples,
+        distribution,
+        noise,
+        missing_data,
+        save_folder,
+    )
+    obj = [objective(x) for x in var]
+    data = np.hstack((var, obj))
+    data = pd.DataFrame(data, columns=var_names + obj_names)
+    filename = filename + ".csv"
+    data.to_csv(filename, index=False)
+    return
+
+
+def generateZDT(
+    problemname: str,
+    num_var: int,
+    num_samples: int,
+    distribution: str,
+    noise: bool,
+    missing_data: bool,
+    save_folder: str,
+):
+    """Generate and save ZDT datasets as csv.
+
+    Parameters
+    ----------
+    problemname : str
+        Name of the problem
+    num_var : int
+        number of variables
+    num_samples : int
+        Number of samples
+    distribution : str
+        Uniform or normal distribution
+    noise : bool
+        Presence or absence of noise
+    missing_data : bool
+        Presence or absence of missing data
+    save_folder : str
+        Path to the folder to save csv files
+    """
+    objective = problems[problemname](num_obj, num_var)
+    var_names = ["x{0}".format(x) for x in range(num_var)]
+    obj_names = ["f1", "f2"]
+    num_var = num_var_zdt[problemname]
+    var, filename = generate_var_0_1(
+        problemname,
+        num_var,
+        num_samples,
+        distribution,
+        noise,
+        missing_data,
+        save_folder,
+    )
+    obj = [objective(x) for x in var]
+    data = np.hstack((var, obj))
+    data = pd.DataFrame(data, columns=var_names + obj_names)
+    filename = filename + ".csv"
+    data.to_csv(filename, index=False)
+    return
